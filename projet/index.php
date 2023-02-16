88905ac08d8a55bb66f9f95866b2292f8cd102da
killyan35 <kilyan.gerard@3wa.io>
2023-02-16 08:59:36 +0000

    fin projet

--- projet/index.php
+++ projet/index.php
@@ -5,11 +5,6 @@ require 'managers/PostManager.php';
 require 'managers/SalonManager.php';
 require'logic/router.php';
 session_start();
-// $um = new CategoryManager("kilyangerard_distorsion","3306","db.3wa.io","kilyangerard","e17f39e5cb4de95dba99a2edd6835ab4");
-// $um1 = new UserManager("kilyangerard_distorsion","3306","db.3wa.io","kilyangerard","e17f39e5cb4de95dba99a2edd6835ab4");
-// $um2 = new PostManager("kilyangerard_distorsion","3306","db.3wa.io","kilyangerard","e17f39e5cb4de95dba99a2edd6835ab4");
-// $um3 = new SalonManager("kilyangerard_distorsion","3306","db.3wa.io","kilyangerard","e17f39e5cb4de95dba99a2edd6835ab4");
-
 
 $newRouter = new Router();
 
@@ -22,8 +17,5 @@ else
     $newRouter->checkRoute("");
 }
 
-// $test = new Category ("test2");
-// $um->insertCategory($test);
-// $id=$um->getCategoryById("2");
-// var_dump($id);
+
 ?>
